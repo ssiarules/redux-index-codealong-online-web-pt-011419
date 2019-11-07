1a86acc6247dd794c9cec45ed897718a2b59e6c3
Johnson Joseph <jhonsonj88@gmail.com>
2019-11-07 03:21:13 +0000

    Automatically backed up by Learn

---
+++ src/components/todos/TodosContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class TodoContainer extends Component {
+
+  render() {
+    return(
+      <div></div>
+    );
+  }
+};
+
+export default connect()(TodosContainer);
